37b280dd97de822f41dd93f8a225e3a4ebc3034c
roxgib <33942237+roxgib@users.noreply.github.com>
2022-11-03 14:41:13 +1100

    Finish class

--- Intro to JS/script.js
+++ Intro to JS/script.js
@@ -1,6 +1,6 @@
 const key = '0b24291856f84ad3b36456e6e7734d43'
 const date = new Date().toISOString().split('T')[0]
-const url = 'https://newsapi.org/v2/everything?' + 
+const url = 'https://newsapi.org/v2/everything?' +
     `q=news&'from=${date}&sortBy=popularity&apiKey=${key}`;
 
 fetch(url)
@@ -9,10 +9,24 @@ fetch(url)
 
 function showArticles(articles) {
     let output = "";
-    
+
     for (article of articles) {
-        article.author
+        if (!article.content.includes("Elon Musk")) {
+            output += `
+        <div class="card">
+            <img
+                src="${article.urlToImage}"
+            />
+            <a class="card-title" href="${article.url}">
+                ${article.title}
+            </a>
+            <div class="card-content">
+                ${article.description}
+            </div>
+        </div>
+        `
+        }
     }
-    
+    console.log(articles[0])
     document.getElementById('container').innerHTML = output;
 }
